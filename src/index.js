b4ccb68b7c6d87a79e119e9037aaa62c3c662446
luizmigueldev <luizmigueln.dev@gmail.com>
2021-08-27 21:37:02 -0300

    Testando servidor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+const express = require("express");
+const app = express();
+const port = 3333;
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.json({
+    mensagem: "API rodando"
+  })
+});
+
+app.listen(port, () => {
+  console.log(`servidor rodando na porta ${port}`)
+})
